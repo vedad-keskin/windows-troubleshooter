19c8c2bf1dac9a215a0a0e8876dee9e8efd362ab
Vedad Keskin <vedad.keskin98@gmail.com>
2025-03-24 01:11:14 +0100

    updated meme code

---
+++ windows-troubleshooter/Project1/Project1/Source.cpp
@@ -0,0 +1,13 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+
+int main() {
+
+	printf("Searching for problems...\n");
+	Sleep(10000);
+	printf("We didn't find any problems\n");
+
+
+	system("PAUSE>0");
+}
